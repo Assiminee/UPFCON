13c8cfe8b3b31238a8e3fd92e90194c2c150f242
Yasmine Znatni <104834084+Assiminee@users.noreply.github.com>
2025-06-01 14:40:24 +0100

    trying to merge

--- UPFCON/Models/Admin.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace UPFCON.Models;
-
-public class Admin : User
-{
-    public IList<Diploma> VerifiedDiplomas { get; set; } = new List<Diploma>();
-}
--- UPFCON/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using UPFCON.Models;
-
-namespace UPFCON;
-
-public class Program
-{
-    public static void Main() { }
-    
-}
