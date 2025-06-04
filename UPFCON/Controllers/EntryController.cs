3bd993c39e37e359e86eda531b071b0cd62ddd6a
Yasmine Znatni <104834084+Assiminee@users.noreply.github.com>
2025-06-04 20:55:34 +0100

    Finished login woohoooo

--- UPFCON/Controllers/EntryController.cs
+++ UPFCON/Controllers/EntryController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace UPFCON.Controllers;
 
 [Route("/Entry")]
+[Authorize]
 public class EntryController : Controller
 {
     // GET
--- UPFCON/Interfaces/IUserService.cs
+++ UPFCON/Interfaces/IUserService.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using UPFCON.Authorization;
 using UPFCON.Models;
 using UPFCON.Models.DTOs;
+using UPFCON.Requests;
 
 namespace UPFCON.Interfaces;
 
@@ -12,4 +14,5 @@ public interface IUserService
     Task<User?> FindUserById(string id);
     Task<IdentityResult> ConfirmUserAsync(User user, string token);
     Task<string> GenerateEmailConfirmationLinkAsync(User user);
+    Task<JwtToken> AuthenticateUser(LoginDto loginDto);
 }
